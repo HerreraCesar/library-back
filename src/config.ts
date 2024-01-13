f65676c47858e4013c61a67b92c93bf762f02096
HerreraCesar <herrera.cesar.arg@gmail.com>
2024-01-13 11:19:47 -0300

    Initializing project

---
+++ src/config.ts
@@ -0,0 +1,17 @@
+import "dotenv/config";
+
+const {
+  PORT,
+  FIREBASE_PROJECT_ID,
+  FIREBASE_CLIENT_EMAIL,
+  FIREBASE_PRIVATE_KEY,
+} = process.env;
+
+export const config = {
+  port: PORT ?? 8080,
+  firebase: {
+    projectId: FIREBASE_PROJECT_ID,
+    clientEmail: FIREBASE_CLIENT_EMAIL,
+    privateKey: FIREBASE_PRIVATE_KEY,
+  },
+};
